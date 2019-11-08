09844ba6c760d6bd40be5474d43bdc2c78724350
yanjie <871531627>
2019-11-08 22:10:57 +0800

    增加：连接本地数据库

--- server/index.js
+++ server/index.js
@@ -7,6 +7,9 @@ const app = express()
 //引入后端路由
 require('./routes/admin')(app)
 
+//引入数据库配置
+require('./plugins/db')(app)
+
 //会在3000端口启动，启动后会调用回调函数
 app.listen(3000, () => {
     console.log('asd');
