c36a2a3d8b9399ddc0f38659ae9c4f1371453b9c
catmmao <catmmao@gmail.com>
2019-11-17 18:32:14 +0800

    增加：mongodb文章模型

---
+++ server/models/Article.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+  //文章标题
+  title: {type: String},
+  //文章正文
+  body: {type: String},
+  //文章分类
+  categories: [{type: mongoose.SchemaTypes.ObjectId, ref: 'Category'}]
+})
+
+module.exports = mongoose.model('Article', schema)
