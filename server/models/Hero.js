6fbbe55ff1ef2afdaaca903e731d649fc0cd2c85
catmmao <catmmao@gmail.com>
2019-11-17 15:05:36 +0800

    修改：完善mongodb英雄模型的字段

--- server/models/Hero.js
+++ server/models/Hero.js
@@ -1,12 +1,76 @@
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
+  //英雄名称
   name: {
     type: String
   },
+  //头像
   avatar: {
     type: String
-  }
+  },
+  //称号
+  title: {
+    type: String
+  },
+  //英雄分类 (单个英雄可能属于多个分类，所以这里使用数组方式)
+  categories: [
+    {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'Category'
+    }
+  ],
+  //评分
+  scores: {
+    //难度
+    difficult: {
+      type: Number
+    },
+    //技能
+    skills: {
+      type: Number
+    },
+    //攻击
+    attack: {
+      type: Number
+    },
+    //生存
+    survive: {
+      type: Number
+    },
+  },
+  //技能
+  skills: [
+    {
+      //技能图标
+      icon: { type: String },
+      //技能名称
+      name: { type: String },
+      //描述
+      description: { type: String },
+      //小提示
+      tips: { type: String }
+    }
+  ],
+  //出装装备（顺风出装）
+  items1: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Items' }],
+  //逆风出装
+  items2: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Items' }],
+  //使用技巧 
+  usageTips: { type: String },
+  //对抗技巧
+  battleTips: { type: String },
+  //团战思路
+  teamTips: { type: String },
+  //英雄关系
+  partners: [
+    {
+      //关联英雄模型
+      hero: {type: mongoose.SchemaTypes.ObjectId, ref: 'Hero'},
+      //描述
+      description: {type: String}
+    }
+  ]
 })
 
 module.exports = mongoose.model('Hero', schema)
