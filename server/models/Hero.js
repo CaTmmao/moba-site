44b3a9ced66075243d03fac7a755010895f405b9
catmmao <catmmao@gmail.com>
2019-11-17 14:19:28 +0800

    增加：mongoodb英雄模型

---
+++ server/models/Hero.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  }
+})
+
+module.exports = mongoose.model('Hero', schema)
