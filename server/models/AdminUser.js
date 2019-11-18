5f8776b82261e43b4f3362fdbe0541ff5ef406de
catmmao <catmmao@gmail.com>
2019-11-18 21:16:31 +0800

    增加：用户管理模型

---
+++ server/models/AdminUser.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+  username: {type: String},
+  password: {type: String}
+})
+
+module.exports = mongoose.model('AdminUser', schema)
