ece11a9448cd722bb013c6294b0444c31c1b4fa0
catmmao <catmmao@gmail.com>
2020-01-27 21:09:51 +0800

    修改：全局过滤器导出方式

--- web/src/util/common.js
+++ web/src/util/common.js
@@ -1,15 +1,13 @@
-export default {
-  /**
-   * 日期转换成 MM/DD
-   * @param {date} date 待转换日期
-   * @returns {string} 转换完的日期
-   */
-  convertDate(date) {
-    date = new Date(date)
-    let months = date.getMonth() + 1
-    months = months < 10 ? `0${months}` : months
-    let days = date.getDate()
+/**
+ * 日期转换成 MM/DD
+ * @param {date} date 待转换日期
+ * @returns {string} 转换完的日期
+ */
+export function convertDate(date) {
+  date = new Date(date)
+  let months = date.getMonth() + 1
+  months = months < 10 ? `0${months}` : months
+  let days = date.getDate()
 
-    return `${months}/${days}`
-  }
+  return `${months}/${days}`
 }
