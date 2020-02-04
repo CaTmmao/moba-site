f12989cff2d4498afa3143dc6dec4e325a0b0a28
catmmao <catmmao@gmail.com>
2020-02-04 15:16:37 +0800

    修改：（后台管理系统）动态生成接口地址

--- admin/src/ajax.js
+++ admin/src/ajax.js
@@ -3,38 +3,39 @@ import router from './router'
 import axios from 'axios'
 
 const http = axios.create({
-    baseURL: 'http://localhost:3001/admin/api/'
+  // 加了斜杠，前缀默认为域名地址  
+  baseURL: process.env.VUE_APP_BASE_URL || '/admin/api'
 })
 
 http.interceptors.request.use(function (config) {
-    if (localStorage.token) {
-        config.headers.Authorization = `Bearer ${localStorage.token}`
-    }
-    return config;
+  if (localStorage.token) {
+    config.headers.Authorization = `Bearer ${localStorage.token}`
+  }
+  return config;
 }, function (error) {
-    return Promise.reject(error);
+  return Promise.reject(error);
 });
 
 http.interceptors.response.use((res) => {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    const { code } = res.data
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  const { code } = res.data
 
-    if (code !== 1) {
-        const { msg } = res.data
-        
-        if (msg) {
-            Message.error(msg)
-        }
+  if (code !== 1) {
+    const { msg } = res.data
 
-        if (code === 110) {
-            router.push('/login')
-        }
+    if (msg) {
+      Message.error(msg)
     }
 
-    return res;
+    if (code === 110) {
+      router.push('/login')
+    }
+  }
+
+  return res;
 }, (err) => {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    return Promise.reject(err);
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  return Promise.reject(err);
 });
 
 export default http
