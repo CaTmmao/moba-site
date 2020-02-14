fdd573324a90385e89ec46c96c8ce8399a7a2d2b
catmmao <catmmao@gmail.com>
2020-02-14 18:18:52 +0800

    修改：（web-英雄详情页）完善英雄详情页

---
+++ web/src/views/Hero/components/card.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="panel pt-d3 px-d3 pb-d1">
+    <div class="flex ai-center pb-d25">
+      <img v-if="icon" class="width-d26 height-d26" :src="icon" />
+      <h2 class="fs-d32 pl-d1">{{title}}</h2>
+    </div>
+    <slot></slot>
+  </div>
+</template> 
+
+<script>
+export default {
+  props: [
+    // icon 图片地址
+    "icon",
+    // 标题
+    "title"
+  ]
+};
+</script>
+
+<style lang="scss" scoped>
+.panel {
+  background: #fff;
+  margin-bottom: 0.22rem;
+  border-bottom: 1px solid #d4d9de;
+}
+</style>
--- web/src/views/Hero/index.vue
+++ web/src/views/Hero/index.vue
@@ -9,11 +9,12 @@
       <p>攻略战</p>
     </div>
 
+    <!-- banner -->
     <div class="banner-wrapper position-re flex fd-column jc-end height-3d82">
       <img :src="data.banner" class="banner-bg" :style="banner" />
       <div class="text-white info position-ab position-l-d3 position-b-0">
         <p class="fs-d22">{{data.title}}</p>
-        <h2 class="fs-d34">{{data.name}}</h2>
+        <p class="fs-d34">{{data.name}}</p>
         <p v-if="data.categories" class="fs-d22">{{data.categories.map(v => v.name).join('/')}}</p>
         <div v-if="data.scores" class="flex scores pb-d24">
           <p>
@@ -35,10 +36,124 @@
         </div>
       </div>
     </div>
+
+    <!-- 技能 -->
+    <Card>
+      <div class="skills">
+        <div class="flex jc-around">
+          <img
+            ref="skillImg"
+            v-for="(item, index) in data.skills"
+            :src="item.icon"
+            class="width-1d2"
+            @click="switchSkill(index)"
+            :class="{'active': index === 0}"
+          />
+        </div>
+        <div
+          class="fs-d26 pt-d4"
+          v-for="(item, index) in data.skills"
+          v-if="index === showSkillIndex"
+        >
+          <div>
+            <strong class="fs-d3 width-1d5 title">{{item.name}}</strong>
+            <span class="text-666">(冷却值：{{item.delay}} 消耗：{{item.cost}})</span>
+          </div>
+          <p v-text="item.description" class="pt-d39 pb-d2"></p>
+        </div>
+      </div>
+    </Card>
+
+    <!-- 出装推荐 -->
+    <Card icon="https://moba-site-1257106288.cos.ap-chengdu.myqcloud.com/items.png" title="出装推荐">
+      <div class="items items1 mb-d25">
+        <p class="fs-d3 pb-d25">顺风出装</p>
+        <div class="flex jc-between">
+          <div v-for="item in data.items1" class="flex fd-column">
+            <img :src="item.icon" class="width-d93 height-d93" />
+            <p class="fs-d18 flex jc-center mt-d1 pb-d1">{{item.name}}</p>
+          </div>
+        </div>
+      </div>
+      <div class="items mb-d25">
+        <p class="fs-d3 pb-d25">逆风出装</p>
+        <div class="flex jc-between">
+          <div v-for="item in data.items2" class="flex fd-column">
+            <img :src="item.icon" class="width-d93 height-d93" />
+            <p class="fs-d18 flex jc-center mt-d1">{{item.name}}</p>
+          </div>
+        </div>
+      </div>
+    </Card>
+
+    <!-- 铭文推荐 -->
+    <Card icon="https://moba-site-1257106288.cos.ap-chengdu.myqcloud.com/rune.png" title="铭文推荐">
+      <div class="flex jc-between runeList">
+        <div v-for="item in data.rune" class="flex">
+          <img class="height-d79" :src="item.icon" />
+          <div class="rune-text ws-nowrap">
+            <p class="fs-d3">{{item.name}}</p>
+            <p v-for="desc in item.desc" class="fs-d18">{{desc.text}}</p>
+          </div>
+        </div>
+      </div>
+    </Card>
+
+    <!-- 使用技巧 -->
+    <Card
+      icon="https://moba-site-1257106288.cos.ap-chengdu.myqcloud.com/usageTips.png"
+      title="使用技巧"
+    >
+      <p class="fs-d26">{{data.usageTips}}</p>
+    </Card>
+
+    <!-- 对抗技巧 -->
+    <Card
+      icon="https://moba-site-1257106288.cos.ap-chengdu.myqcloud.com/battleTips.png"
+      title="对抗技巧"
+    >
+      <p class="fs-d26">{{data.battleTips}}</p>
+    </Card>
+
+    <!-- 团战思路 -->
+    <Card icon="https://moba-site-1257106288.cos.ap-chengdu.myqcloud.com/teamTips.png" title="团战思路">
+      <p class="fs-d26">{{data.teamTips}}</p>
+    </Card>
+
+    <!-- 英雄关系 -->
+    <Card icon="https://moba-site-1257106288.cos.ap-chengdu.myqcloud.com/relation.png" title="英雄关系">
+      <div class="heroRelate">
+        <!-- 最佳搭档 -->
+        <div>
+          <p>最佳搭档</p>
+          <div v-for="item in data.partners">
+            <img :src="item.hero.avatar" />
+            <p>{{item.description}}</p>
+          </div>
+        </div>
+        <!-- 被谁克制 -->
+        <div>
+          <p>被谁克制</p>
+          <div v-for="item in data.controledBy">
+            <img :src="item.hero.avatar" />
+            <p>{{item.description}}</p>
+          </div>
+        </div>
+        <!-- 克制谁 -->
+        <div>
+          <p>克制谁</p>
+          <div v-for="item in data.control">
+            <img :src="item.hero.avatar" />
+            <p>{{item.description}}</p>
+          </div>
+        </div>
+      </div>
+    </Card>
   </div>
 </template>
 
 <script>
+import Card from "./components/card";
 export default {
   name: "heroes",
   props: {
@@ -48,7 +163,9 @@ export default {
   data() {
     return {
       // 英雄详情信息
-      data: {}
+      data: {},
+      // 当前显示哪个技能
+      showSkillIndex: 0
     };
   },
   mounted() {
@@ -64,6 +181,9 @@ export default {
       };
     }
   },
+  components: {
+    Card
+  },
   methods: {
     // 获取英雄详细信息
     getHeroInfo() {
@@ -73,6 +193,17 @@ export default {
         this.data = res.data;
         console.log(this.data);
       });
+    },
+    /**
+     * 切换技能
+     * @param {number} index 索引
+     */
+    switchSkill(index) {
+      this.$refs.skillImg.forEach(item => {
+        item.classList.remove("active");
+      });
+      this.$refs.skillImg[index].classList.add("active");
+      this.showSkillIndex = index;
     }
   }
 };
@@ -80,6 +211,18 @@ export default {
 
 <style lang="scss" scoped>
 .hero-detail {
+  @keyframes txtshow {
+    from {
+      opacity: 0;
+      transform: translateX(-100px);
+    }
+
+    to {
+      opacity: 1;
+      transform: none;
+    }
+  }
+
   .banner-wrapper {
     background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1));
 
@@ -88,11 +231,18 @@ export default {
     }
 
     .info {
+      > p {
+        animation: txtshow 1s;
+        animation-fill-mode: forwards;
+        opacity: 0;
+      }
       .scores {
         p {
           display: flex;
           align-items: center;
           font-size: 0.24rem;
+          animation: txtshow 1s forwards;
+          opacity: 0;
 
           span {
             border-radius: 50%;
@@ -104,9 +254,73 @@ export default {
             display: inline-flex;
             justify-content: center;
             align-items: center;
-            margin-right: .1rem;
-            margin-left: .02rem;
+            margin-right: 0.1rem;
+            margin-left: 0.02rem;
           }
+        }
+      }
+    }
+  }
+
+  // 铭文推荐
+  .runeList {
+    .rune-text {
+      margin-left: 9%;
+    }
+  }
+
+  //出装推荐
+  .items {
+    img {
+      border-radius: 0.46rem;
+    }
+
+    &.items1 {
+      border-bottom: 1px solid #d4d9de;
+    }
+  }
+
+  //技能
+  .skills {
+    .title {
+      display: inline-block;
+    }
+    img {
+      box-sizing: border-box;
+      border-radius: 0.58rem;
+      border: 0.05rem solid transparent;
+      &.active {
+        border-color: #d59b40;
+      }
+    }
+  }
+
+  //英雄关系
+  .heroRelate {
+    > div {
+      font-size: 0.26rem;
+      margin-bottom: 0.2rem;
+      &:not(:last-child) {
+        border-bottom: 1px solid #d4d9de;
+      }
+
+      > p {
+        font-size: 0.3rem;
+        padding-bottom: 0.25rem;
+      }
+
+      > div {
+        padding-bottom: 0.3rem;
+        display: flex;
+        justify-content: space-between;
+
+        img {
+          width: 0.96rem;
+          height: 0.96rem;
+        }
+
+        p {
+          width: 5.86rem;
         }
       }
     }
