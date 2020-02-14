e19d8ebb69ad7ad147c59b3a587b556930f96dda
catmmao <catmmao@gmail.com>
2020-02-14 18:40:56 +0800

    修改：（web）点击图标回到首页

--- web/src/views/Main/index.vue
+++ web/src/views/Main/index.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
     <div class="topbar flex bg-black ai-center height-d9">
-      <img src="@/assets/logo.png" class="ml-d3 width-d7" />
+      <router-link to="/">
+        <img src="@/assets/logo.png" class="ml-d3 width-d7" />
+      </router-link>
       <div class="fg-1 pl-d2 height-d7 flex fd-column jc-start">
         <p class="text-white fs-d26 pb-dd3">王者荣耀</p>
-        <p class="text-666 fs-d16 ">团队成就更多</p>
+        <p class="text-666 fs-d16">团队成就更多</p>
       </div>
       <button type="button" class="btn bg-primary width-1d2 height-d48 mr-d3">
         <a class="text-333 fw-bold" href="https://pvp.qq.com/zlkdatasys/mct/d/play.shtml">立即下载</a>
