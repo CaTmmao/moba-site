1b221aeff5f1f0f439b25103198bf9afe7fa93b2
catmmao <catmmao@gmail.com>
2020-03-17 18:45:55 +0800

    修改：（web-全部英雄页面）布局

--- web/src/views/Main/Home/components/allHeroes.vue
+++ web/src/views/Main/Home/components/allHeroes.vue
@@ -11,13 +11,17 @@
       >{{item.name}}</span>
     </div>
     <div class="px-d24">
-      <div v-for="(item, index) in heroList" v-if="index === showHeroIndex" class="mt-d32 flex">
+      <div
+        v-for="(item, index) in heroList"
+        v-if="index === showHeroIndex"
+        class="mt-d32 flex flex-wrap hero"
+      >
         <router-link
           tag="div"
           :to="`/heroes/${hero._id}`"
           v-for="(hero, index) in item.heroList"
           :key="index"
-          class="width-1d12 height-1d6 mr-d35 mb-d28 flex fd-column"
+          class="width-1d12 height-1d6 mr-d35 mb-d28 flex fd-column hero-item"
         >
           <img :src="hero.avatar" class="height-1d12" />
           <p class="flex jc-center fs-d24 mt-d1">{{hero.name}}</p>
@@ -68,3 +72,9 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.hero .hero-item:nth-child(5n) {
+  margin-right: 0;
+}
+</style>
